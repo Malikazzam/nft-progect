9f749e0175e5129e3c86b3f3b22a791a0da9f1ed
Malikazzam <89993849+Malikazzam@users.noreply.github.com>
2021-12-14 15:50:05 +0200

    first commit

---
+++ src/components/Punklist.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { CollectionCard } from './CollectionCard'
+import './Punklist.css'
+
+const Punklist = ({ punklistData, setSelectedPunk }) => {
+    return (
+        <div className='punkList'>
+            {punklistData.map(punk => (
+                <div onClick={() => setSelectedPunk(punk.token_id)}>
+                    <CollectionCard 
+                    key={punk.token_id}
+                    id={punk.token_id}
+                    name={punk.name}
+                    traits={punk.traits}
+                    image={punk.image_original_url}
+                    />
+                </div>
+            ))}
+            
+        </div>
+    )
+}
+
+export default Punklist
